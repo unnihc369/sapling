fec6ea9780acb91dca48311a7e656e76c4df67af
unnihc <unnihc369@gmail.com>
2023-12-17 22:44:03 +0530

    added img

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -4,6 +4,8 @@ import { Link, useNavigate } from "react-router-dom";
 import { ToastError, ToastSuccess } from "../utility/Toasts";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../Firebase";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Login = () => {
   const [email, setEmail] = useState("");
@@ -45,64 +47,70 @@ const Login = () => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-primary">
-      <div className="w-full max-w-xs">
-        <form className="bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <div className="mb-4">
-            <label
-              className="block text-white text-sm font-bold mb-2"
-              htmlFor="username"
-            >
-              Email
-            </label>
-            <input
-              className="shadow appearance-none border rounded bg-gray-800 w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline focus:bg-gray-800"
-              id="username"
-              type="text"
-              placeholder="registed email"
-              value={email}
-              onChange={handleUsernameChange}
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-white text-sm font-bold mb-2"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-white mb-3 leading-tight focus:outline-none bg-gray-800 focus:shadow-outline focus:bg-gray-800"
-              id="password"
-              type="password"
-              placeholder="******************"
-              value={password}
-              onChange={handlePasswordChange}
-            />
-            <p className="text-red-500 text-xs italic">
-              Please choose a password.
-            </p>
-          </div>
-          <div className="flex items-center justify-between">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
-              onClick={handleSignIn}
-            >
-              Sign In
-            </button>
-          </div>
-          <div className="mt-4">
-            <Link to="/signup" className="text-white">
-              <span className="py-4">Already have an account?</span>
-            </Link>
-          </div>
-        </form>
-        <p className="text-center text-gray-500 text-xs">
-          &copy;2020 Acme Corp. All rights reserved.
-        </p>
+    <>
+      <Header />
+      <div className="flex justify-center items-center h-screen bg-primary">
+        <div className="w-full max-w-xs">
+          <form className="bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
+              <label
+                className="block text-white text-sm font-bold mb-2"
+                htmlFor="username"
+              >
+                Email
+              </label>
+              <input
+                className="shadow appearance-none border rounded bg-gray-800 w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline focus:bg-gray-800"
+                id="username"
+                type="text"
+                placeholder="registed email"
+                value={email}
+                onChange={handleUsernameChange}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-white text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-white mb-3 leading-tight focus:outline-none bg-gray-800 focus:shadow-outline focus:bg-gray-800"
+                id="password"
+                type="password"
+                placeholder="******************"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+              <p className="text-red-500 text-xs italic">
+                Please choose a password.
+              </p>
+            </div>
+            <div className="flex items-center justify-between">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+                onClick={handleSignIn}
+              >
+                Sign In
+              </button>
+            </div>
+            <div className="mt-4">
+              <Link to="/signup" className="text-white">
+                <span className="py-4">Already have an account?</span>
+              </Link>
+            </div>
+          </form>
+          <p className="text-center text-gray-500 text-xs">
+            &copy;2020 Acme Corp. All rights reserved.
+          </p>
+        </div>
       </div>
-    </div>
+      <div className="bg-gray-500 p-6">
+        <Footer />
+      </div>
+    </>
   );
 };
 
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 // this are toast to display warnings and informations
 import { ToastError, ToastSuccess, ToastWarning } from "../utility/Toasts";
@@ -46,83 +48,89 @@ const Signup = () => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-primary">
-      <div className="w-full max-w-xs">
-        <form className="bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <div className="mb-4">
-            <label
-              className="block text-white text-sm font-bold mb-2"
-              htmlFor="username"
-            >
-              Email
-            </label>
-            <input
-              className="shadow appearance-none border rounded bg-gray-800 w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline focus:bg-gray-800"
-              id="username"
-              type="email"
-              placeholder="your Email secure with us"
-              value={email}
-              onChange={handleUsernameChange}
-            />
-          </div>
-          <div className="mb-4">
-            <label
-              className="block text-white text-sm font-bold mb-2"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <input
-              className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-white mb-3 leading-tight focus:outline-none bg-gray-800 focus:shadow-outline focus:bg-gray-800"
-              id="password"
-              type="password"
-              placeholder="******************"
-              value={password}
-              onChange={handlePasswordChange}
-            />
-            <p className="text-red-500 text-xs italic">
-              Please choose a password.
-            </p>
-          </div>
-          {/* confirm password */}
+    <>
+    <Header/>
+      <div className="flex justify-center items-center h-screen bg-primary mt-4">
+        <div className="w-full max-w-xs">
+          <form className="bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
+              <label
+                className="block text-white text-sm font-bold mb-2"
+                htmlFor="username"
+              >
+                Email
+              </label>
+              <input
+                className="shadow appearance-none border rounded bg-gray-800 w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline focus:bg-gray-800"
+                id="username"
+                type="email"
+                placeholder="your Email secure with us"
+                value={email}
+                onChange={handleUsernameChange}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-white text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Password
+              </label>
+              <input
+                className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-white mb-3 leading-tight focus:outline-none bg-gray-800 focus:shadow-outline focus:bg-gray-800"
+                id="password"
+                type="password"
+                placeholder="******************"
+                value={password}
+                onChange={handlePasswordChange}
+              />
+              <p className="text-red-500 text-xs italic">
+                Please choose a password.
+              </p>
+            </div>
+            {/* confirm password */}
 
-          <div className="mb-4">
-            <label
-              className="block text-white text-sm font-bold mb-2"
-              htmlFor="password"
-            >
-              Confirm Password
-            </label>
-            <input
-              className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-white mb-3 leading-tight focus:outline-none bg-gray-800 focus:shadow-outline focus:bg-gray-800"
-              id="password"
-              type="password"
-              placeholder="******************"
-              value={confirPassword}
-              onChange={handleCofirmPasswordChange}
-            />
-          </div>
+            <div className="mb-4">
+              <label
+                className="block text-white text-sm font-bold mb-2"
+                htmlFor="password"
+              >
+                Confirm Password
+              </label>
+              <input
+                className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-white mb-3 leading-tight focus:outline-none bg-gray-800 focus:shadow-outline focus:bg-gray-800"
+                id="password"
+                type="password"
+                placeholder="******************"
+                value={confirPassword}
+                onChange={handleCofirmPasswordChange}
+              />
+            </div>
 
-          <div className="flex items-center justify-between">
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-              type="button"
-              onClick={handleSignIn}
-            >
-              Sign In
-            </button>
-          </div>
-          <div className="mt-4">
-            <Link to="/login" className="text-white">
-              <span className="py-4">Have have an account?</span>
-            </Link>
-          </div>
-        </form>
-        <p className="text-center text-gray-500 text-xs">
-          &copy;2020 Acme Corp. All rights reserved.
-        </p>
+            <div className="flex items-center justify-between">
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="button"
+                onClick={handleSignIn}
+              >
+                Sign In
+              </button>
+            </div>
+            <div className="mt-4">
+              <Link to="/login" className="text-white">
+                <span className="py-4">Have have an account?</span>
+              </Link>
+            </div>
+          </form>
+          <p className="text-center text-gray-500 text-xs">
+            &copy;2020 Acme Corp. All rights reserved.
+          </p>
+        </div>
       </div>
-    </div>
+      <div className="bg-gray-500 p-6">
+        <Footer />
+      </div>
+    </>
   );
 };
 
