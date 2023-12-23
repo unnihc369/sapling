4314d4f190d7ea9144b458d8daf32927d31bede4
Praveen kumar G <110656664+praveen-all@users.noreply.github.com>
2023-12-23 22:49:16 +0530

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -53,19 +53,32 @@ const Navbar = () => {
             !toggle ? "hidden" : "flex"
           } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
         >
-          <ul className="list-none flex justify-end items-start flex-1 flex-col">
-            {navLinks.map((nav, index) => (
-              <li
-                key={nav.id}
-                className={`font-poppins font-medium cursor-pointer text-[16px] ${
-                  active === nav.title ? "text-white" : "text-dimWhite"
-                } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
-                onClick={() => setActive(nav.title)}
-              >
-                <a href={`#${nav.id}`}>{nav.title}</a>
-              </li>
-            ))}
-          </ul>
+          {user ? (
+            <ul className="list-none flex justify-end items-start flex-1 flex-col">
+              {navLinks.map((nav, index) => (
+                <li
+                  key={nav.id}
+                  className={`font-poppins font-medium cursor-pointer text-[16px] ${
+                    active === nav.title ? "text-white" : "text-dimWhite"
+                  } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
+                  onClick={() => setActive(nav.title)}
+                >
+                  <NavLink to={`/${nav.id}`}>{nav.title}</NavLink>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <ul className="list-none flex justify-end items-start flex-1 flex-col">
+             <li
+                  
+                  className={`font-poppins font-medium cursor-pointer text-[16px] text-white`}
+                 
+                >
+                  <NavLink to={`/login`}>Login</NavLink>
+                </li>
+              ))
+            </ul>
+          )}
         </div>
       </div>
     </motion.nav>
