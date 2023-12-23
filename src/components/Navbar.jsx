552fa6984d5c1f3dafae61732bfbed40f726acf9
Praveen kumar G <110656664+praveen-all@users.noreply.github.com>
2023-12-23 23:01:00 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -63,7 +63,7 @@ const Navbar = () => {
                   } ${index === navLinks.length - 1 ? "mb-0" : "mb-4"}`}
                   onClick={() => setActive(nav.title)}
                 >
-                  <NavLink to={`/${nav.id}`}>{nav.title}</NavLink>{user.isFarmer === "true" && nav.id === "farms" ? (
+                 {user.isFarmer === "true" && nav.id === "farms" ? (
                     <NavLink to={`/addfarm`}>addfarm</NavLink>
                   ) : (
                     <NavLink to={`/${nav.id}`}>{nav.title}</NavLink>
